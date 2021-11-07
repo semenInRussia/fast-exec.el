4ebdbbe9efdf309ed299bfdaa9a0c2706d1f96c8
semenInRussia <hrams205@gmail.com>
2021-11-07 15:50:46 +0300

    Delete support of projectile

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ((eval . (progn
-                   (require 'projectile)
-                   (puthash (projectile-project-root)
-                            "emacs -batch -f package-initialize -L . -f buttercup-run-discover"
-                            projectile-test-cmd-map))))))
